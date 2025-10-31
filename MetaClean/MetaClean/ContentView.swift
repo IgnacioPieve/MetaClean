12917b66a98d66a2eaab19419033117fe1e6c3de
Ignacio Pieve Roiger <ignacio.pieve@gmail.com>
2025-10-30 21:24:45 -0300

    cursor: empty commit to allow worktree creation

---
+++ MetaClean/MetaClean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetaClean
+//
+//  Created by Ignacio Pieve Roiger on 30/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetaClean/MetaClean/MetaCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaCleanApp.swift
+//  MetaClean
+//
+//  Created by Ignacio Pieve Roiger on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MetaCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
